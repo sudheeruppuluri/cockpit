58d6523172814d0b194c1d5c80150195dec26ee6
Sudheer Uppuluri <sudheer.uppuluri@gmail.com>
2020-09-23 18:30:33 +0530

    table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,93 @@ import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { TableListComponent } from './table-list/table-list.component';
+// import { TypographyComponent } from './typography/typography.component';
+// import { IconsComponent } from './icons/icons.component';
+// import { MapsComponent } from './maps/maps.component';
+// import { NotificationsComponent } from './notifications/notifications.component';
+// import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDatepickerModule} from '@angular/material/datepicker'
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+// import {MatExpansionModule} from '@angular/material/esm2015';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
+
+import {BrowserModule} from '@angular/platform-browser';
+// import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+// import {TableFilteringExample} from './app/table-filtering-example';
+
 @NgModule({
+    exports: [
+      BrowserModule,
+      MatAutocompleteModule,
+      MatBadgeModule,
+      MatBottomSheetModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatCardModule,
+      MatCheckboxModule,
+      MatChipsModule,
+      MatDatepickerModule,
+      MatDialogModule,
+      MatDividerModule,
+      MatGridListModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      MatMenuModule,
+      MatPaginatorModule,
+      MatProgressBarModule,
+      MatProgressSpinnerModule,
+      MatRadioModule,
+      MatSelectModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatSlideToggleModule,
+      MatSnackBarModule,
+      MatSortModule,
+      MatTableModule,
+      MatTabsModule,
+      MatToolbarModule,
+      MatTooltipModule,
+      MatTreeModule,
+      
+    ],
   imports: [
     BrowserAnimationsModule,
     FormsModule,
@@ -43,5 +116,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // entryComponents: [TableFilteringExample],
+  // declarations: [TableFilteringExample],
 })
 export class AppModule { }
--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { COMMA, TAB, SPACE, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
+
+export interface PeriodicElement {
+  vesselid: number;
+  vesselname: string;
+  vesselcode: string;
+  vesseltype:  string;
+  maxteus: number;
+  cargotype: string;
+  status: boolean;
+}
+
+export interface SearchItem {
+  name: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { vesselid: 123, vesselname: 'Khang Shun', vesselcode: 'M6X6', vesseltype: 'Dry Bulk', maxteus: 8000, cargotype: 'Coal', status: true },
+   { vesselid: 124, vesselname: 'Niran Shun', vesselcode: 'M6X6', vesseltype: 'Navy', maxteus: 8000, cargotype: 'Coal', status: true },
+    { vesselid: 125, vesselname: 'Shun Shun', vesselcode: 'M6X6', vesseltype: 'Tug', maxteus: 8000, cargotype: 'Coal', status: true },
+     { vesselid: 126, vesselname: 'Grei Shun', vesselcode: 'M6X6', vesseltype: 'Dry Bulk', maxteus: 8000, cargotype: 'Coal', status: true },
+      { vesselid: 127, vesselname: 'Wang Shun', vesselcode: 'M6X6', vesseltype: 'Dry Bulk', maxteus: 8000, cargotype: 'Coal', status: true },
+  
+];
 
 @Component({
   selector: 'app-table-list',
@@ -9,7 +36,92 @@ export class TableListComponent implements OnInit {
 
   constructor() { }
 
+  displayedColumns: string[] = ['vesselid', 'vesselname', 'vesselcode', 'vesseltype', 'maxteus', 'cargotype', 'status'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  positionFilter = new FormControl();
+  nameFilter = new FormControl();
+  private filterValues = { id: '', name: '' }
+
+  filteredValues = {
+    position: '', name: '', weight: '',
+    symbol: '', topFilter: false
+  };
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [COMMA, TAB, ENTER];
+  searchItems: SearchItem[] = [];
+
   ngOnInit() {
+
+    this.positionFilter.valueChanges.subscribe((positionFilterValue) => {
+      console.log(positionFilterValue);
+
+      this.filteredValues['position'] = positionFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+      this.filteredValues['topFilter'] = false;
+    });
+
+
+    this.nameFilter.valueChanges
+      .subscribe(value => {
+        this.filterValues['name'] = value
+        this.dataSource.filter = JSON.stringify(this.filterValues)
+      });
+    this.dataSource.filterPredicate = this.createFilter();
+  }
+
+    add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    console.log('event', event);
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.searchItems.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(item: SearchItem): void {
+    const index = this.searchItems.indexOf(item);
+
+    if (index >= 0) {
+      this.searchItems.splice(index, 1);
+    }
+  }
+
+
+  applyFilter(filterValue: string) {
+    let filter = {
+      name: filterValue.trim().toLowerCase(),
+      position: filterValue.trim().toLowerCase(),
+      topFilter: true
+    }
+    this.dataSource.filter = JSON.stringify(filter)
+  }
+
+  createFilter() {
+    let filterFunction = function (data: any, filter: string): boolean {
+      let searchTerms = JSON.parse(filter)
+      let idSearch = data.id.toString().indexOf(searchTerms.id) != -1
+      let nameSearch = () => {
+        let found = false;
+        searchTerms.name.trim().toLowerCase().split(' ').forEach(word => {
+          if (data.name.toLowerCase().indexOf(word) != -1) { found = true }
+        });
+        return found
+      }
+      return idSearch && nameSearch()
+    }
+    return filterFunction
   }
 
 }
